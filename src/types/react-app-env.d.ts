c0b26e41c6d7cb91029b5c606477a0a3316e941d
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-09 19:24:59 +0100

    Centralize custom React types (#24)

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -1,2 +1,39 @@
+// Image types
+
 declare module "*.png";
 declare module "*.jpg";
+
+// Common FC Props types
+
+interface AppProps {
+  applicationName: string;
+}
+
+interface NavbarProps {
+  image: string;
+  pages: Page[];
+}
+
+// Page FC Props types
+
+interface PageProps {
+  pageName: string;
+}
+
+// Collection types
+
+interface Pages {
+  MAIN_PAGE: Page;
+  PAGE_1: Page;
+  PAGE_2: Page;
+  ERROR_PAGE: Page;
+}
+
+// Custom data model types
+
+// Other custom types
+
+interface Page {
+  name: string;
+  path: string;
+}
