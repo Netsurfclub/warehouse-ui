4b40a4037aaaafdd3cef568a73d84faa6f861e99
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-09 12:28:02 +0100

    Change image.

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import robotImage from "../../images/robot.jpg";
+import image from "../../images/unplugged_cable.jpg";
 
 const ErrorPage: React.FC<ErrorPageProps> = ({ mainPagePath }) => {
   return (
     <div className="row">
       <div className="col-12">
         <div className="text-center">
-          <img
-            src={robotImage}
-            alt="logo"
-            style={{ width: "45%", height: "45%" }}
-          />
+          <img src={image} alt="logo" style={{ width: "45%", height: "45%" }} />
           <h1>Hiba történt!</h1>
           <Link
             className="btn btn-primary"
