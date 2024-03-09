237a2b7fe8c9fe27e74c137ca6c3e4f656096b4f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-09 13:09:18 +0100

    Remove label tag.

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -3,20 +3,18 @@ import React from "react";
 const Card: React.FC<CardProps> = ({ title, image, imageAlt, contents }) => {
   return (
     <div className="card mt-3 mb-3">
-      <label htmlFor="photo-upload">
-        <img
-          src={image}
-          alt={imageAlt}
-          className="card-img-top"
-          style={{
-            width: "200px",
-            height: "200px",
-            display: "block",
-            margin: "0 auto",
-            objectFit: "contain",
-          }}
-        />
-      </label>
+      <img
+        src={image}
+        alt={imageAlt}
+        className="card-img-top"
+        style={{
+          width: "200px",
+          height: "200px",
+          display: "block",
+          margin: "0 auto",
+          objectFit: "contain",
+        }}
+      />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         {contents.map((content: KeyValuePair<string, string>) => (
