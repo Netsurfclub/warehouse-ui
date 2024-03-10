2a68df9b41ace0c82f6a9d0bfa1c1af19371dd3f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-10 15:01:29 +0100

    Merge constant files into one file.

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,1 +1,13 @@
-export const APPLICATION_NAME: string = "Warehouse";
+const APPLICATION_NAME: string = "Warehouse";
+
+const INVENTORY_PAGE: KeyValuePair<string, string> = {
+  key: "Raktárkészlet",
+  value: "/inventory",
+};
+
+const ERROR_PAGE: KeyValuePair<string, string> = {
+  key: "Error",
+  value: "/error",
+};
+
+export { APPLICATION_NAME, INVENTORY_PAGE, ERROR_PAGE };
